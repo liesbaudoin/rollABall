c986de73626d837bd96a86e81a697c2d8a53fa52
Liesbeth Baudoin <LiesBaudoin@hotmail.com>
2018-10-31 16:26:08 +0100

    added camera controllers and walls

--- Roll a ball/Assets/scripts/playerController.cs
+++ Roll a ball/Assets/scripts/playerController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour {
 
 private Rigidbody rb;
 public float speed;
